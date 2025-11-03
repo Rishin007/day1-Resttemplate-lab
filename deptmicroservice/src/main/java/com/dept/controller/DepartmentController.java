01750f32dfb17c311159dfe1265b142deed507ca
Rishin Dutta <123900701+Rishin007@users.noreply.github.com>
2025-11-03 15:06:10 +0530

    Update DepartmentController.java
    Controller fixed

--- deptmicroservice/src/main/java/com/dept/controller/DepartmentController.java
+++ deptmicroservice/src/main/java/com/dept/controller/DepartmentController.java
@@ -21,21 +21,9 @@ public class DepartmentController {
         DepartmentDto departmentDto1 = departmentService.saveDepartment(departmentDto);
         return new  ResponseEntity<>(departmentDto1, HttpStatus.CREATED);
     }
-    @GetMapping
-    public ResponseEntity<List<DepartmentDto>> getAllDepartments() {
-        return new ResponseEntity<>(departmentService.findAllDepartments(), HttpStatus.OK);
-    }
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
         return new ResponseEntity<>(departmentDto, HttpStatus.OK);
-    }
-    @PutMapping("{code}")
-    public ResponseEntity<DepartmentDto> updateDepartment(@PathVariable String code, @RequestBody DepartmentDto departmentDto) {
-        return new ResponseEntity<>(departmentService.updateDepartment(code, departmentDto), HttpStatus.OK);
-    }
-    @DeleteMapping("{departmentCode}")
-    public ResponseEntity<String> deleteDepartment(@PathVariable String departmentCode){
-        return new ResponseEntity<>(departmentService.deleteDepartmentByCode(departmentCode),HttpStatus.OK);
     }
 }
