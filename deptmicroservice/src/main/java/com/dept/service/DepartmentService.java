eecce3da706c3ab842ebdda01e682d94b211666a
Rishin Dutta <123900701+Rishin007@users.noreply.github.com>
2025-11-03 15:03:59 +0530

    Update DepartmentService.java
    Extra methods removed

--- deptmicroservice/src/main/java/com/dept/service/DepartmentService.java
+++ deptmicroservice/src/main/java/com/dept/service/DepartmentService.java
@@ -6,8 +6,5 @@ import java.util.List;
 
 public interface DepartmentService {
     DepartmentDto saveDepartment(DepartmentDto departmentDto);
-    List<DepartmentDto> findAllDepartments();
     DepartmentDto getDepartmentByCode(String code);
-    DepartmentDto updateDepartment(String code,DepartmentDto departmentDto);
-    String deleteDepartmentByCode(String code);
 }
